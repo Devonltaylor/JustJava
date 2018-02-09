a2de432790e41ac1e853ed106e3f04bf2789625b
Devonltaylor <devonltaylor22@gmail.com>
2018-02-08 20:59:52 -0500

    6th Commit: Intergers and strings added to display message and price.

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -33,9 +33,8 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-
-        display(quantity);
-        displayPrice(quantity * 5);
+        String priceMessage = "Amount Due: " + "$" + (quantity * 5) + ".00";
+        displayMessage(priceMessage);
     }
 
     /**
@@ -70,5 +69,13 @@ public class MainActivity extends AppCompatActivity {
     private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
     }
 }
